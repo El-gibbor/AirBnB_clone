710dfb4686ed79c09d66ae191958f518bbdf4111
El-gibbor <chiagoziem11@yahoo.com>
2023-03-11 23:55:41 +0400

    ressolved unboundlocalerror on timestamp assingment

--- models/base_model.py
+++ models/base_model.py
@@ -26,11 +26,12 @@ class BaseModel:
         # self.created_at = self.updated_at = datetime.now()
         #   def __init__(self, *args, **kwargs):
         if kwargs:
+            time = None
+            kwargs.pop("__class__")
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key != '__class__':
-                    setattr(self, key, value)
+                if key in ('created_at','updated_at'):
+                    time = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, time)
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
@@ -56,3 +57,25 @@ class BaseModel:
             else:
                 inst_dict[keyz] = valz
         return inst_dict
+
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)
