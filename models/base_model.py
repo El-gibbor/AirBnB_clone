fbf1dd4c08cfd284268a1f166b8ae6cbf629b533
El-gibbor <chiagoziem11@yahoo.com>
2023-03-12 00:13:58 +0400

    task 4. all resolved and good. hopefully mr checker will behave

--- models/base_model.py
+++ models/base_model.py
@@ -27,11 +27,11 @@ class BaseModel:
         #   def __init__(self, *args, **kwargs):
         if kwargs:
             time = None
+            kwargs.pop("__class__")
             for key, value in kwargs.items():
                 if key in ('created_at','updated_at'):
                     time = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    if key != "__class__":
-                        setattr(self, key, time)
+                setattr(self, key, time)
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
@@ -58,24 +58,24 @@ class BaseModel:
                 inst_dict[keyz] = valz
         return inst_dict
 
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-print(my_model.id)
-print(my_model)
-print(type(my_model.created_at))
-print("--")
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+# my_model = BaseModel()
+# my_model.name = "My_First_Model"
+# my_model.my_number = 89
+# print(my_model.id)
+# print(my_model)
+# print(type(my_model.created_at))
+# print("--")
+# my_model_json = my_model.to_dict()
+# print(my_model_json)
+# print("JSON of my_model:")
+# for key in my_model_json.keys():
+#     print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
 
-print("--")
-my_new_model = BaseModel(**my_model_json)
-print(my_new_model.id)
-print(my_new_model)
-print(type(my_new_model.created_at))
+# print("--")
+# my_new_model = BaseModel(**my_model_json)
+# print(my_new_model.id)
+# print(my_new_model)
+# print(type(my_new_model.created_at))
 
-print("--")
-print(my_model is my_new_model)
+# print("--")
+# print(my_model is my_new_model)
