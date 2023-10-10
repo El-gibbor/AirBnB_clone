cc3113886950fb5cc9730c64ee9991aec1639dda
El-gibbor <chiagoziem11@yahoo.com>
2023-10-10 20:35:36 +0400

    implemented instance recreation from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -11,12 +11,22 @@ class BaseModel:
     """ Defines a class Basemodel from which its subclasses will
     inherit from.
     """
-    def __init__(self) -> None:
+    def __init__(self, *args, **kwargs):
         """ initialises all public instance attributes """
 
-        self.id = "{}".format(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            del kwargs["__class__"]
+            for keys, val in kwargs.items():
+                if "created_at" in keys or "updated_at" in keys:
+                    # convert its value (previously a str) to datetime object
+                    dt_time = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, keys, dt_time)
+                else:
+                    setattr(self, keys, val)
+        else:
+            self.id = "{}".format(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self) -> str:
         """ returns a string representaion of the class and
@@ -33,4 +43,16 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """ returns dictionary representation"""
+        """ returns dictionary representation containing all attr of
+        an instance. a key __class__ with the class name of the
+        obj is added """
+
+        dict_repr = {}
+        dict_repr["__class__"] = "{}".format(type(self).__name__)
+        for keys, val in self.__dict__.items():
+            if "created_at" in keys or "updated_at" in keys:
+                dict_repr[keys] = val.isoformat()
+            else:
+                dict_repr[keys] = val
+        return dict_repr
+    
