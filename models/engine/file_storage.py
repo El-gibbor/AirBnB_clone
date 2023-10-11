a1604695d35135e9d6a7ca12a103513bc9d36f7c
El-gibbor <chiagoziem11@yahoo.com>
2023-10-11 18:40:28 +0400

    pep validated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,36 +4,54 @@ Abstracting this from the BaseModel makes it independent so that we
 can easily replace the storage system without re-coding everything everywhere.
 """
 from json import dump, load
-from models.base_model import BaseModel
 
 
 class FileStorage:
-    """ This class serializes instances to a JSON file and deserializes
-    JSON file to instances """
-
+    """defines a class for data interchange stream.
+    Attributes:
+        __file_path: path to the JSON file
+        __objects - (dictionary): storage for all objects
+    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """ returns the dictionary __objects """
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """ Stores the given object in the __objects dictionary, using
         a unique key generated from the object's class name and ID attribute.
         This key is used for easy retrieval of the object later.
         """
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        FileStorage.__objects[key] = obj
+        FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
     def save(self):
-        """ serializes __objects to the JSON file (path: __file_path) """
-
-        dict_repr = [value.to_dict for value in FileStorage.__objects.values()]
-        with open(FileStorage.__file_path, 'w') as json_stream:
-            dump(dict_repr, json_stream)
+        """ serializes __objects to the JSON file (path: __file_path)
+        """
+        all_objs = FileStorage.__objects
+        to_dict_repr = {k: v.to_dict() for k, v in all_objs.items()}
+        with open(FileStorage.__file_path, "w") as json_stream:
+            dump(to_dict_repr, json_stream)
 
     def reload(self):
         """deserializes the JSON file to __objects
         (only if the JSON file (__file_path)
         """
+        from models.base_model import BaseModel
+
+        # gate all defined classes mapped to a name str in this name space
+        defined_classes = {"BaseModel": BaseModel}
+
+        try:
+            with open(FileStorage.__file_path) as json_str:
+                deserialised = load(json_str)
+                for obj_values in deserialised.values():
+                    cls_name = obj_values["__class__"]
+                    cls_obj = defined_classes[cls_name]
+                    self.new(eval(cls_name)(**obj_values))
+        except FileNotFoundError:
+            pass
+
+
+
