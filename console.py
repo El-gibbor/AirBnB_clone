cdb5aa1bc94a80f80f1ae17b7e139469020e5e7c
El-gibbor <chiagoziem11@yahoo.com>
2023-10-12 00:10:21 +0400

    implemented create() to create and print obj id on console

--- console.py
+++ console.py
@@ -3,16 +3,17 @@
 mamipulate the bojects of our web application
 """
 import cmd
-
-
+from models.base_model import BaseModel
 class HBNBCommand(cmd.Cmd):
     """ Defines a class for managing command-line interface for the project """
+
+    defined_classes = {"BaseModel"}
 
     prompt = "(hbnb) "
 
     def emptyline(self):
         """ overrides default empty line behaviour so that commands dont get
-        re-executed when you press enter after an execution. """
+        re-executed when you press enter after an execution."""
         pass
 
     def do_quit(self, arg_line):
@@ -24,6 +25,20 @@ class HBNBCommand(cmd.Cmd):
         print()
         return True
 
+    def do_create(self, arg_line):
+        """ creates a new instance, saves it (to the json file)
+        and prints the id
+        """
+        cmd_list = arg_line.split(" ")
+        if cmd_list[0] == "":
+            print("** class name missing **")
+        else:
+            if cmd_list[0] == "BaseModel":
+                obj_1 = BaseModel()
+                obj_1.save()
+                print(obj_1.id)
+            else:
+                print("** class doesn't exist **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
