2bc782ce1a9314a78be8e1cc1d6802e27291c085
El-gibbor <chiagoziem11@yahoo.com>
2023-10-12 10:13:58 +0400

    confirming a few things with this file

--- xperiment.py
+++ xperiment.py
@@ -9,6 +9,6 @@ class myClass:
 # in tthe reload() method in file_storage, I used the ouput from this to know
 # what should be accepted from eval() after it evaluates the name str as an expression
 
-print(type(myClass)) # prints <class type>  -> means its of type data type or class
+print(type(myClass)) # prints <class type>  -> means tthat it's of type data-type or class
 
-print(type(5))  # prints <class 'int'>  -> means its of int data type or class
+print(type(5))  # prints <class 'int'>  -> means that it's of int data-type or class
