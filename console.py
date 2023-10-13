6efe792ae9af4c044f22c6b08fc2511070c0d718
El-gibbor <chiagoziem11@yahoo.com>
2023-10-13 16:34:18 +0400

    finally got do_create() working with the helper func

--- console.py
+++ console.py
@@ -27,28 +27,6 @@ class HBNBCommand(cmd.Cmd):
         print()
         return True
 
-    # def do_create(self, args):
-    #     """ creates a new instance, saves it (to the json file)
-    #     and prints the id
-    #     """
-    #     if not args:
-    #         print("** class name missing **")
-    #     else:
-    #         cls_name = args.split()[0]
-    #         if cls_name == "BaseModel":
-    #             new_obj = storage.cls_map()[cls_name]()
-    #             new_obj.save()
-    #             print(new_obj.id)
-    #         else:
-    #             print("** class doesn't exist **")
-    #         # TODO: are spaces handled as missing or non-existing argument?
-
-    # def do_create(self, args):
-    #         clss_name = self.does_it_exist(args)
-    #         new_obj = storage.cls_map()[clss_name]()
-    #         new_obj.save()
-    #         print(new_obj.id)
-
     def does_it_exist(self, args):
         """ helper method to validate the existence of the given class name
             in the storage object's class mapping.
@@ -57,22 +35,20 @@ class HBNBCommand(cmd.Cmd):
         Return:
             str: If class name exists in the class mapping, the function returns
                     the class name. If the class name is missing or not found,
-                    it prints an error message repectively.
+                    it prints the respective error message.
         """
         if not args:
             print("** class name missing **")
         else:
-            clss_attr = args.split()
-            if clss_attr[0] not in storage.cls_map():
+            cls_name = args.split()[0]
+            if cls_name not in storage.cls_map():
                 print("** class doesn't exist **")
-            if clss_attr[1] != storage.cls_map()[clss_attr[0].id]:
-                print("** class doesn't exist *")
             else:
-                return clss_attr[0]
+                return cls_name
 
     def do_create(self, args):
         """ creates a new instance, saves it (to the json file)
-             and prints the id
+            and prints the id
         """
         cls_name = self.does_it_exist(args)
         if cls_name:
@@ -85,7 +61,16 @@ class HBNBCommand(cmd.Cmd):
         on the class name and id
         """
         cls_name = self.does_it_exist(args)
-
+        if cls_name:
+            cls_attr = args.split()
+            if len(cls_attr) < 2:
+                print("** instance id missing **")
+            else:
+                obj_key = "{}.{}".format(cls_name, cls_attr[1])
+                try:
+                    print(storage.all()[obj_key])
+                except KeyError:
+                    print("** no instance found **")
 
 
 if __name__ == '__main__':
