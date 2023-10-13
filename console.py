be8d216a3b258a34affbe0a6741dfebf710b2dc2
El-gibbor <chiagoziem11@yahoo.com>
2023-10-13 18:10:31 +0400

    got the id helper function working properly

--- console.py
+++ console.py
@@ -12,6 +12,43 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
 
+    # ******************** HELPER FUNCTIONS *********************
+
+    def validate_cls_name(self, args):
+        """ helper method to validate the existence of the given class name
+            in the storage object's class mapping.
+        Args:
+            args (str): class name obtained from user input.
+        Return:
+            str: The class name. otherwise, prints the respective error message
+                    If the class name is missing or not found.
+        """
+        if not args:
+            print("** class name missing **")
+        else:
+            cls_name = args.split()[0]
+            if cls_name not in storage.cls_map():
+                print("** class doesn't exist **")
+            else:
+                return cls_name
+
+    def validate_cls_id(self, args):
+        """ Validates the presence of an instance ID in the provided arguments.
+         Args:
+            args (str): The input string containing class name and instance ID.
+        Returns:
+            list: A list containing the split arguments if the instance ID
+                      is provided; otherwise, prints an error message.
+    """
+        arguments = args.split()
+        if len(arguments) < 2:
+            print("** instance id missing **")
+            # can return None here for further usecase to validat a mising id elsewhere
+        else:
+            return arguments
+
+    # ************************ CONSOLE COMMANDS ***********************
+
     def emptyline(self):
         """ overides default emptyline execution """
         pass
@@ -27,46 +64,22 @@ class HBNBCommand(cmd.Cmd):
         print()
         return True
 
-    def does_it_exist(self, args):
-        """ helper method to validate the existence of the given class name
-            in the storage object's class mapping.
-        Args:
-            args (str): class name obtained from user input.
-        Return:
-            str: If class name exists in the class mapping, the function returns
-                    the class name. If the class name is missing or not found,
-                    it prints the respective error message.
-        """
-        if not args:
-            print("** class name missing **")
-        else:
-            cls_name = args.split()[0]
-            if cls_name not in storage.cls_map():
-                print("** class doesn't exist **")
-            else:
-                return cls_name
-
     def do_create(self, args):
-        """ creates a new instance, saves it (to the json file)
-            and prints the id
-        """
-        cls_name = self.does_it_exist(args)
+        """ Create, save (to the JSON file) and print the id of a new instance."""
+        cls_name = self.validate_cls_name(args)
         if cls_name:
             new_obj = storage.cls_map()[cls_name]()
             new_obj.save()
             print(new_obj.id)
 
     def do_show(self, args):
-        """ Prints the string representation of an instance based
-        on the class name and id
-        """
-        cls_name = self.does_it_exist(args)
+        """ Prints string representation of an instance based
+        on the class name and id. """
+        cls_name = self.validate_cls_name(args)
         if cls_name:
-            cls_attr = args.split()
-            if len(cls_attr) < 2:
-                print("** instance id missing **")
-            else:
-                obj_key = "{}.{}".format(cls_name, cls_attr[1])
+            cls_id = self.validate_cls_id(args)
+            if cls_id:
+                obj_key = "{}.{}".format(cls_name, args.split()[1])
                 try:
                     print(storage.all()[obj_key])
                 except KeyError:
